e33bf10b1854a0cb2f157dc6240da88f3df39f34
vorges-data <vinicius.jborges36@gmail.com>
2021-04-19 21:47:29 -0300

    dados agrupados, dias para o respectivo mês

--- Coronavirus.R
+++ Coronavirus.R
@@ -14,8 +14,12 @@ dados <- read.csv('dados/brazil_covid19.csv') %>%
   mutate(date=ymd(dados$date)) %>%
   dplyr::select(date,cases) %>% #Selecionando colunas data e nº de casos
   mutate(Moving.Average10=rollmean(cases,k=10,fill=NA)) #Média movel 10 dias
+  agrupados <- group_by(dados,year(date),month(date)) #Agrupar os dias aos meses
+  summarise(agrupados,sum(cases))
+  g <- summarise(agrupados,sum(cases))
+  
 
-ggplot(dados,aes(x=date,y=cases)) + #Definindo os valores dos dados
+ggplot(dados,aes(x=date,y=cases + #Definindo os valores dos dados
   geom_line(aes(col='Cases',lty='Cases'),lwd=0.8) +
   geom_line(aes(y=Moving.Average10,col= 'Moving Average 10 days',
                 lty='Moving Average 10 days'),lwd=0.8) + #Media movel
